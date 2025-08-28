8d33bdb8c76b0d99c6605762f24c485f564ea790
Sadanand Lowanshi <sadanand@appliedthoughts.com>
2025-08-28 11:44:40 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.hexalitics.multiimagepicker"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
